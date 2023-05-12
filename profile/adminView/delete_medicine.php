85b325711dca30d83ab27f03b5f5159f281d76b8
Ahmed Abdel-Fattah <47077630+ahmedabdelfattah50@users.noreply.github.com>
2023-05-12 21:01:03 +0300

    delete comments

--- profile/adminView/delete_medicine.php
+++ profile/adminView/delete_medicine.php
@@ -1,11 +1,5 @@
 <?php
   include_once "../db/connection.php";
-  
-  // delete medicine page @##@#!@
-  // 213
-  // 123
-  // 123
-
   $medicine_id = $_POST['medicine_id'];
   $sql = "SELECT * from medicine where id = '".$medicine_id."'";
   $res = mysqli_query($conn, $sql);
